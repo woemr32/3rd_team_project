8a0f1d5fe7d2e63dc59161aa32762515ec9c3bd4
user <user@user-PC>
2017-08-03 18:44:18 +0900

    sdf

---
+++ 3rd_team/src/kr/co/sist/ssangbang/user/calendar/controller/CalendarController.java
@@ -0,0 +1,18 @@
+package kr.co.sist.ssangbang.user.calendar.controller;
+
+import java.util.List;
+
+import kr.co.sist.ssangbang.user.calendar.domain.Schedule;
+//import kr.co.sist.ssangbang.user.calendar.service.CalendarService;
+
+
+public class CalendarController {
+	
+//	private CalendarService cs;
+	
+	public List<Schedule> searchAllSchedule(String id){
+		List<Schedule> list=null;
+		
+		return list;
+	}//searchAllSchedule
+}
