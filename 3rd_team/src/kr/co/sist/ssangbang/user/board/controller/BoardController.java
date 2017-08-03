8737eed29a28c85952fa1ade53f7dbfe6c3a64e3
user <user@user-PC>
2017-08-03 11:12:00 +0900

    libs, annotation

--- 3rd_team/src/kr/co/sist/ssangbang/user/board/controller/BoardController.java
+++ 3rd_team/src/kr/co/sist/ssangbang/user/board/controller/BoardController.java
@@ -3,8 +3,25 @@ package kr.co.sist.ssangbang.user.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @Controller
 public class BoardController {
 	
+	@RequestMapping(value="cover.do",method=GET)
+	public String goCover(){
+		
+		
+		return "cover/cover";
+	}//goCover
 	
-}
+	
+	@RequestMapping(value="/index.do",method=GET)
+	public String gomain(){
+		
+		
+		return "main/index";
+	}//goCover
+
+}//class
